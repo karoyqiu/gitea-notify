86627cfc55b379032a6acdff3fdd7642b9aa1e06
Roy QIU <karoyqiu@gmail.com>
2026-02-18 10:16:45 +0800

    实现简单的通知。

---
+++ src-tauri/src/gitea.rs
@@ -0,0 +1,270 @@
+use reqwest::{Client, Method, Proxy, Result};
+use serde::{Deserialize, Serialize};
+
+/// Repository : Repository represents a repository
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Repository {
+  // #[serde(rename = "allow_fast_forward_only_merge", skip_serializing_if = "Option::is_none")]
+  // pub allow_fast_forward_only_merge: Option<bool>,
+  // #[serde(rename = "allow_manual_merge", skip_serializing_if = "Option::is_none")]
+  // pub allow_manual_merge: Option<bool>,
+  // #[serde(rename = "allow_merge_commits", skip_serializing_if = "Option::is_none")]
+  // pub allow_merge_commits: Option<bool>,
+  // #[serde(rename = "allow_rebase", skip_serializing_if = "Option::is_none")]
+  // pub allow_rebase: Option<bool>,
+  // #[serde(rename = "allow_rebase_explicit", skip_serializing_if = "Option::is_none")]
+  // pub allow_rebase_explicit: Option<bool>,
+  // #[serde(rename = "allow_rebase_update", skip_serializing_if = "Option::is_none")]
+  // pub allow_rebase_update: Option<bool>,
+  // #[serde(rename = "allow_squash_merge", skip_serializing_if = "Option::is_none")]
+  // pub allow_squash_merge: Option<bool>,
+  // #[serde(rename = "archived", skip_serializing_if = "Option::is_none")]
+  // pub archived: Option<bool>,
+  // #[serde(rename = "archived_at", skip_serializing_if = "Option::is_none")]
+  // pub archived_at: Option<String>,
+  // #[serde(rename = "autodetect_manual_merge", skip_serializing_if = "Option::is_none")]
+  // pub autodetect_manual_merge: Option<bool>,
+  // #[serde(rename = "avatar_url", skip_serializing_if = "Option::is_none")]
+  // pub avatar_url: Option<String>,
+  // #[serde(rename = "clone_url", skip_serializing_if = "Option::is_none")]
+  // pub clone_url: Option<String>,
+  // #[serde(rename = "created_at", skip_serializing_if = "Option::is_none")]
+  // pub created_at: Option<String>,
+  // #[serde(rename = "default_allow_maintainer_edit", skip_serializing_if = "Option::is_none")]
+  // pub default_allow_maintainer_edit: Option<bool>,
+  // #[serde(rename = "default_branch", skip_serializing_if = "Option::is_none")]
+  // pub default_branch: Option<String>,
+  // #[serde(rename = "default_delete_branch_after_merge", skip_serializing_if = "Option::is_none")]
+  // pub default_delete_branch_after_merge: Option<bool>,
+  // #[serde(rename = "default_merge_style", skip_serializing_if = "Option::is_none")]
+  // pub default_merge_style: Option<String>,
+  // #[serde(rename = "description", skip_serializing_if = "Option::is_none")]
+  // pub description: Option<String>,
+  // #[serde(rename = "empty", skip_serializing_if = "Option::is_none")]
+  // pub empty: Option<bool>,
+  // #[serde(rename = "external_tracker", skip_serializing_if = "Option::is_none")]
+  // pub external_tracker: Option<Box<models::ExternalTracker>>,
+  // #[serde(rename = "external_wiki", skip_serializing_if = "Option::is_none")]
+  // pub external_wiki: Option<Box<models::ExternalWiki>>,
+  // #[serde(rename = "fork", skip_serializing_if = "Option::is_none")]
+  // pub fork: Option<bool>,
+  // #[serde(rename = "forks_count", skip_serializing_if = "Option::is_none")]
+  // pub forks_count: Option<i64>,
+  //#[serde(rename = "full_name", skip_serializing_if = "Option::is_none")]
+  pub full_name: String,
+  // #[serde(rename = "has_actions", skip_serializing_if = "Option::is_none")]
+  // pub has_actions: Option<bool>,
+  // #[serde(rename = "has_code", skip_serializing_if = "Option::is_none")]
+  // pub has_code: Option<bool>,
+  // #[serde(rename = "has_issues", skip_serializing_if = "Option::is_none")]
+  // pub has_issues: Option<bool>,
+  // #[serde(rename = "has_packages", skip_serializing_if = "Option::is_none")]
+  // pub has_packages: Option<bool>,
+  // #[serde(rename = "has_projects", skip_serializing_if = "Option::is_none")]
+  // pub has_projects: Option<bool>,
+  // #[serde(rename = "has_pull_requests", skip_serializing_if = "Option::is_none")]
+  // pub has_pull_requests: Option<bool>,
+  // #[serde(rename = "has_releases", skip_serializing_if = "Option::is_none")]
+  // pub has_releases: Option<bool>,
+  // #[serde(rename = "has_wiki", skip_serializing_if = "Option::is_none")]
+  // pub has_wiki: Option<bool>,
+  #[serde(rename = "html_url", skip_serializing_if = "Option::is_none")]
+  pub html_url: Option<String>,
+  #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
+  pub id: Option<i64>,
+  // #[serde(rename = "ignore_whitespace_conflicts", skip_serializing_if = "Option::is_none")]
+  // pub ignore_whitespace_conflicts: Option<bool>,
+  // #[serde(rename = "internal", skip_serializing_if = "Option::is_none")]
+  // pub internal: Option<bool>,
+  // #[serde(rename = "internal_tracker", skip_serializing_if = "Option::is_none")]
+  // pub internal_tracker: Option<Box<models::InternalTracker>>,
+  // #[serde(rename = "language", skip_serializing_if = "Option::is_none")]
+  // pub language: Option<String>,
+  // #[serde(rename = "languages_url", skip_serializing_if = "Option::is_none")]
+  // pub languages_url: Option<String>,
+  // #[serde(rename = "licenses", skip_serializing_if = "Option::is_none")]
+  // pub licenses: Option<Vec<String>>,
+  // #[serde(rename = "link", skip_serializing_if = "Option::is_none")]
+  // pub link: Option<String>,
+  // #[serde(rename = "mirror", skip_serializing_if = "Option::is_none")]
+  // pub mirror: Option<bool>,
+  // #[serde(rename = "mirror_interval", skip_serializing_if = "Option::is_none")]
+  // pub mirror_interval: Option<String>,
+  // #[serde(rename = "mirror_updated", skip_serializing_if = "Option::is_none")]
+  // pub mirror_updated: Option<String>,
+  // #[serde(rename = "name", skip_serializing_if = "Option::is_none")]
+  // pub name: Option<String>,
+  // /// ObjectFormatName of the underlying git repository
+  // #[serde(rename = "object_format_name", skip_serializing_if = "Option::is_none")]
+  // pub object_format_name: Option<ObjectFormatName>,
+  // #[serde(rename = "open_issues_count", skip_serializing_if = "Option::is_none")]
+  // pub open_issues_count: Option<i64>,
+  // #[serde(rename = "open_pr_counter", skip_serializing_if = "Option::is_none")]
+  // pub open_pr_counter: Option<i64>,
+  // #[serde(rename = "original_url", skip_serializing_if = "Option::is_none")]
+  // pub original_url: Option<String>,
+  // #[serde(rename = "owner", skip_serializing_if = "Option::is_none")]
+  // pub owner: Option<Box<models::User>>,
+  // #[serde(rename = "parent", skip_serializing_if = "Option::is_none")]
+  // pub parent: Option<Box<models::Repository>>,
+  // #[serde(rename = "permissions", skip_serializing_if = "Option::is_none")]
+  // pub permissions: Option<Box<models::Permission>>,
+  // #[serde(rename = "private", skip_serializing_if = "Option::is_none")]
+  // pub private: Option<bool>,
+  // #[serde(rename = "projects_mode", skip_serializing_if = "Option::is_none")]
+  // pub projects_mode: Option<String>,
+  // #[serde(rename = "release_counter", skip_serializing_if = "Option::is_none")]
+  // pub release_counter: Option<i64>,
+  // #[serde(rename = "repo_transfer", skip_serializing_if = "Option::is_none")]
+  // pub repo_transfer: Option<Box<models::RepoTransfer>>,
+  // #[serde(rename = "size", skip_serializing_if = "Option::is_none")]
+  // pub size: Option<i64>,
+  // #[serde(rename = "ssh_url", skip_serializing_if = "Option::is_none")]
+  // pub ssh_url: Option<String>,
+  // #[serde(rename = "stars_count", skip_serializing_if = "Option::is_none")]
+  // pub stars_count: Option<i64>,
+  // #[serde(rename = "template", skip_serializing_if = "Option::is_none")]
+  // pub template: Option<bool>,
+  // #[serde(rename = "topics", skip_serializing_if = "Option::is_none")]
+  // pub topics: Option<Vec<String>>,
+  // #[serde(rename = "updated_at", skip_serializing_if = "Option::is_none")]
+  // pub updated_at: Option<String>,
+  // #[serde(rename = "url", skip_serializing_if = "Option::is_none")]
+  // pub url: Option<String>,
+  // #[serde(rename = "watchers_count", skip_serializing_if = "Option::is_none")]
+  // pub watchers_count: Option<i64>,
+  // #[serde(rename = "website", skip_serializing_if = "Option::is_none")]
+  // pub website: Option<String>,
+}
+
+/// ActionWorkflowRun : ActionWorkflowRun represents a WorkflowRun
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct ActionWorkflowRun {
+  // #[serde(rename = "actor", skip_serializing_if = "Option::is_none")]
+  // pub actor: Option<Box<models::User>>,
+  #[serde(rename = "completed_at", skip_serializing_if = "Option::is_none")]
+  pub completed_at: Option<String>,
+  #[serde(rename = "conclusion", skip_serializing_if = "Option::is_none")]
+  pub conclusion: Option<String>,
+  #[serde(rename = "display_title", skip_serializing_if = "Option::is_none")]
+  pub display_title: Option<String>,
+  // #[serde(rename = "event", skip_serializing_if = "Option::is_none")]
+  // pub event: Option<String>,
+  // #[serde(rename = "head_branch", skip_serializing_if = "Option::is_none")]
+  // pub head_branch: Option<String>,
+  // #[serde(rename = "head_repository", skip_serializing_if = "Option::is_none")]
+  // pub head_repository: Option<Box<models::Repository>>,
+  // #[serde(rename = "head_sha", skip_serializing_if = "Option::is_none")]
+  // pub head_sha: Option<String>,
+  #[serde(rename = "html_url", skip_serializing_if = "Option::is_none")]
+  pub html_url: Option<String>,
+  #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
+  pub id: Option<i64>,
+  // #[serde(rename = "path", skip_serializing_if = "Option::is_none")]
+  // pub path: Option<String>,
+  // #[serde(rename = "repository", skip_serializing_if = "Option::is_none")]
+  // pub repository: Option<Box<models::Repository>>,
+  // #[serde(rename = "repository_id", skip_serializing_if = "Option::is_none")]
+  // pub repository_id: Option<i64>,
+  #[serde(rename = "run_attempt", skip_serializing_if = "Option::is_none")]
+  pub run_attempt: Option<i64>,
+  //#[serde(rename = "run_number", skip_serializing_if = "Option::is_none")]
+  pub run_number: i64,
+  #[serde(rename = "started_at", skip_serializing_if = "Option::is_none")]
+  pub started_at: Option<String>,
+  //#[serde(rename = "status", skip_serializing_if = "Option::is_none")]
+  pub status: String,
+  // #[serde(rename = "trigger_actor", skip_serializing_if = "Option::is_none")]
+  // pub trigger_actor: Option<Box<models::User>>,
+  // #[serde(rename = "url", skip_serializing_if = "Option::is_none")]
+  // pub url: Option<String>,
+}
+
+/// ActionWorkflowRunsResponse : ActionWorkflowRunsResponse returns ActionWorkflowRuns
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct ActionWorkflowRunsResponse {
+  #[serde(rename = "total_count", skip_serializing_if = "Option::is_none")]
+  pub total_count: Option<i64>,
+  #[serde(rename = "workflow_runs", skip_serializing_if = "Option::is_none")]
+  pub workflow_runs: Option<Vec<ActionWorkflowRun>>,
+}
+
+/// Gitea API
+pub struct Gitea {
+  pub base_url: String,
+  pub token: String,
+  client: Client,
+}
+
+impl Gitea {
+  pub fn new(base_url: String, token: String) -> Self {
+    // let client  = Client::builder().proxy(Proxy::all("127.0.0.1:9000").unwrap()).build().unwrap();
+    Self {
+      base_url: format!("{}/api/v1", base_url.trim_end_matches('/')),
+      token,
+      client: Client::new(),
+    }
+  }
+
+  pub fn reset(&mut self, base_url: String, token: String) {
+    self.base_url = format!("{}/api/v1", base_url.trim_end_matches('/'));
+    self.token = token;
+  }
+
+  pub async fn user_current_list_repos(
+    &self,
+    page: Option<i32>,
+    limit: Option<i32>,
+  ) -> Result<Vec<Repository>> {
+    let url = format!("{}/user/repos", self.base_url);
+    let mut req = self
+      .client
+      .request(Method::GET, url)
+      .bearer_auth(&self.token);
+
+    if let Some(page) = page {
+      req = req.query(&[("page", page.to_string())]);
+    }
+
+    if let Some(limit) = limit {
+      req = req.query(&[("limit", limit.to_string())]);
+    }
+
+    let req = req.build()?;
+    self
+      .client
+      .execute(req)
+      .await?
+      .json::<Vec<Repository>>()
+      .await
+  }
+
+  pub async fn get_workflow_runs(
+    &self,
+    repo: &Repository,
+    page: Option<i32>,
+    limit: Option<i32>,
+  ) -> Result<ActionWorkflowRunsResponse> {
+    let url = format!("{}/repos/{}/actions/runs", self.base_url, repo.full_name);
+    let mut req = self
+      .client
+      .request(Method::GET, url)
+      .bearer_auth(&self.token);
+
+    if let Some(page) = page {
+      req = req.query(&[("page", page.to_string())]);
+    }
+
+    if let Some(limit) = limit {
+      req = req.query(&[("limit", limit.to_string())]);
+    }
+
+    let req = req.build()?;
+    self
+      .client
+      .execute(req)
+      .await?
+      .json::<ActionWorkflowRunsResponse>()
+      .await
+  }
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,10 @@
-use std::sync::Mutex;
+mod gitea;
+
 use std::time::Duration;
 use std::{collections::HashMap, sync::LazyLock};
 
 use reqwest;
 use serde::Deserialize;
-use serde_json::Value;
 use tauri::{
   AppHandle, Manager, Result, WebviewWindowBuilder,
   menu::{Menu, MenuItem, PredefinedMenuItem},
@@ -12,7 +12,13 @@ use tauri::{
 };
 use tauri_plugin_notification::NotificationExt;
 use tauri_plugin_store::{self as plugin_store, StoreExt};
-use tokio;
+use tokio::{sync::Mutex, time};
+
+use crate::gitea::Gitea;
+
+/// Gitea API
+static GITEA: LazyLock<Mutex<Gitea>> =
+  LazyLock::new(|| Mutex::new(Gitea::new("".to_string(), "".to_string())));
 
 /// 进程内已见 run 状态缓存（不持久化）
 /// Key: "{owner/repo}#{run_id}" -> last seen state string
@@ -47,236 +53,53 @@ fn read_settings(app: &AppHandle) -> Option<Settings> {
   None
 }
 
-/// 通过插件发送通知（优先）或回退到系统通知（notify-rust via plugin or native）
-fn notify_via_plugin_or_fallback(app: &AppHandle, title: &str, body: &str) {
-  // Using plugin notification API exposed on AppHandle
-  // The plugin provides `app.notification()` via trait `NotificationExt` (per sample).
-  // We call builder and show; if it errors, fallback to a best-effort local notify using the plugin itself if available.
-  let app = app.clone();
-  let title = title.to_string();
-  let body = body.to_string();
+async fn check_repo_workflow_runs(app: &AppHandle, gitea: &Gitea) -> reqwest::Result<()> {
+  let repos = gitea.user_current_list_repos(None, Some(100)).await?;
 
-  // We call `app.notification()` (provided by the plugin via the trait).
-  // Use `.builder()` -> `.title()`/`.body()` -> `.show()`.
-  // This call may return a Result; ignore/show errors gracefully.
-  if let Err(e) = app
-    .notification()
-    .builder()
-    .title(&title)
-    .body(&body)
-    .show()
-  {
-    eprintln!("plugin notification show error: {:?}", e);
-  }
-}
+  // TODO: 仓库白名单
 
-/// 列出用户仓库（owner/repo 列表），若失败返回空 vec
-async fn list_repos_from_gitea(
-  base_url: &str,
-  token: &str,
-  client: &reqwest::Client,
-) -> Vec<String> {
-  let url = format!("{}/api/v1/user/repos", base_url.trim_end_matches('/'));
-  let resp = match client.get(&url).bearer_auth(token).send().await {
-    Ok(r) => r,
-    Err(e) => {
-      eprintln!("failed to request repos: {:?}", e);
-      return Vec::new();
-    }
-  };
+  let mut known_runs = KNOWN_RUNS.lock().await;
+  let mut lines: Vec<String> = Vec::new();
 
-  if !resp.status().is_success() {
-    eprintln!("repos request returned status: {}", resp.status());
-    return Vec::new();
-  }
+  for repo in repos {
+    // 只取最近一次执行
+    let runs = gitea.get_workflow_runs(&repo, Some(1), Some(1)).await?;
 
-  let mut out = Vec::new();
-  match resp.json::<Value>().await {
-    Ok(v) => {
-      if let Some(arr) = v.as_array() {
-        for repo in arr {
-          if let Some(fn_) = repo.get("full_name").and_then(|s| s.as_str()) {
-            out.push(fn_.to_string());
-          } else {
-            let owner = repo
-              .get("owner")
-              .and_then(|o| o.get("login"))
-              .and_then(|s| s.as_str())
-              .unwrap_or("");
-            let name = repo.get("name").and_then(|s| s.as_str()).unwrap_or("");
-            if !owner.is_empty() && !name.is_empty() {
-              out.push(format!("{}/{}", owner, name));
-            }
+    if let Some(runs) = runs.workflow_runs {
+      for run in runs {
+        let key = format!("{}#{}", repo.full_name, run.run_number);
+
+        if let Some(known_status) = known_runs.insert(key, run.status.clone()) {
+          // 已经见过了，比较当前状态与之前的状态
+          if known_status != run.status {
+            lines.push(format!("{} - {}", repo.full_name, run.status));
           }
-        }
-      }
-    }
-    Err(e) => {
-      eprintln!("failed to parse repos json: {:?}", e);
-    }
-  }
-
-  out
-}
-
-/// 拉取单个仓库的 workflow runs 并检测新建/完成事件，发送通知
-async fn poll_repo_runs_with_app(
-  base_url: &str,
-  token: &str,
-  client: &reqwest::Client,
-  app: &AppHandle,
-  repo: &str,
-) {
-  let parts: Vec<&str> = repo.splitn(2, '/').collect();
-  if parts.len() != 2 {
-    return;
-  }
-  let owner = parts[0];
-  let name = parts[1];
-
-  let url = format!(
-    "{}/api/v1/repos/{}/{}/actions/runs",
-    base_url.trim_end_matches('/'),
-    owner,
-    name
-  );
-  let resp = match client.get(&url).bearer_auth(token).send().await {
-    Ok(r) => r,
-    Err(e) => {
-      eprintln!("failed to fetch runs for {}: {:?}", repo, e);
-      return;
-    }
-  };
-
-  if !resp.status().is_success() {
-    eprintln!("runs request for {} returned {}", repo, resp.status());
-    return;
-  }
-
-  let json = match resp.json::<Value>().await {
-    Ok(j) => j,
-    Err(e) => {
-      eprintln!("failed to parse runs json for {}: {:?}", repo, e);
-      return;
-    }
-  };
-
-  // 支持多种字段形式：workflow_runs、runs、或根数组
-  let runs = if let Some(arr) = json.get("workflow_runs").and_then(|v| v.as_array()) {
-    arr.clone()
-  } else if let Some(arr) = json.get("runs").and_then(|v| v.as_array()) {
-    arr.clone()
-  } else if json.is_array() {
-    json.as_array().unwrap().clone()
-  } else {
-    Vec::new()
-  };
-
-  let mut map = KNOWN_RUNS.lock().unwrap();
-
-  for run in runs {
-    let id_opt = run
-      .get("id")
-      .and_then(|v| v.as_i64())
-      .or_else(|| run.get("run_id").and_then(|v| v.as_i64()))
-      .or_else(|| run.get("number").and_then(|v| v.as_i64()));
-
-    if id_opt.is_none() {
-      continue;
-    }
-    let id = id_opt.unwrap().to_string();
-
-    let status = run
-      .get("status")
-      .and_then(|v| v.as_str())
-      .unwrap_or("")
-      .to_string();
-    let conclusion = run
-      .get("conclusion")
-      .and_then(|v| v.as_str())
-      .unwrap_or("")
-      .to_string();
-    let _url_field = run
-      .get("html_url")
-      .and_then(|v| v.as_str())
-      .or_else(|| run.get("url").and_then(|v| v.as_str()))
-      .map(|s| s.to_string());
-
-    let key = format!("{}#{}", repo, id);
-
-    match map.get(&key) {
-      None => {
-        // New run
-        map.insert(key.clone(), status.clone());
-        let title = format!("Workflow started - {}", repo);
-        let body = format!(
-          "Run {} started (status={})",
-          id,
-          if status.is_empty() {
-            "unknown"
-          } else {
-            &status
-          }
-        );
-
-        // send plugin notification (preferred) or fallback
-        notify_via_plugin_or_fallback(app, &title, &body);
-      }
-      Some(prev) => {
-        // 检查从非终态到终态
-        let was_terminal = {
-          let l = prev.to_lowercase();
-          l == "completed" || l == "done" || l == "success" || l == "failure" || l == "cancelled"
-        };
-        let is_terminal = {
-          let s = status.to_lowercase();
-          let c = conclusion.to_lowercase();
-          s == "completed"
-            || s == "done"
-            || c == "success"
-            || c == "failure"
-            || c == "cancelled"
-            || s == "failure"
-            || s == "success"
-        };
-
-        if !was_terminal && is_terminal {
-          map.insert(key.clone(), status.clone());
-          let title = format!("Workflow finished - {}", repo);
-          let body = if !conclusion.is_empty() {
-            format!("Run {} finished: {}", id, conclusion)
-          } else if !status.is_empty() {
-            format!("Run {} finished: {}", id, status)
-          } else {
-            format!("Run {} finished", id)
-          };
-
-          // send notification
-          notify_via_plugin_or_fallback(app, &title, &body);
         } else {
-          // 更新状态
-          map.insert(key.clone(), status.clone());
+          lines.push(format!("{} - {}", repo.full_name, run.status));
         }
       }
     }
   }
+
+  if !lines.is_empty() {
+    // 统一通知
+    let title = String::from("工作流运行状态变更");
+    let body = lines.join("\n");
+    let _ = app.notification().builder().title(title).body(body).show();
+  }
+
+  Ok(())
 }
 
 /// 周期任务主逻辑：从 store 读取配置并轮询所有仓库
 async fn periodic_task_for_app(app: AppHandle) {
   // read settings (try plugin then fallback file)
   if let Some(settings) = read_settings(&app) {
-    let client = reqwest::Client::new();
+    let mut gitea = GITEA.lock().await;
+    gitea.reset(settings.url, settings.token);
 
-    // decide repo list
-    let repos: Vec<String> = if let Some(list) = settings.repos.clone() {
-      list
-    } else {
-      list_repos_from_gitea(&settings.url, &settings.token, &client).await
-    };
-
-    for repo in repos {
-      poll_repo_runs_with_app(&settings.url, &settings.token, &client, &app, &repo).await;
+    if let Err(e) = check_repo_workflow_runs(&app, &gitea).await {
+      eprintln!("Error: {}", e);
     }
   } else {
     eprintln!("settings not found; skipping this tick");
@@ -286,13 +109,13 @@ async fn periodic_task_for_app(app: AppHandle) {
 /// 启动周期任务。把 owned AppHandle 移动到后台任务，以便在任务中使用 plugin API。
 fn start_periodic_task_with_app(app_handle: AppHandle) {
   tauri::async_runtime::spawn(async move {
-    let mut interval = tokio::time::interval(Duration::from_millis(PERIODIC_INTERVAL_MILLIS));
+    let mut interval = time::interval(Duration::from_millis(PERIODIC_INTERVAL_MILLIS));
 
     loop {
       interval.tick().await;
 
       let fut = periodic_task_for_app(app_handle.clone());
-      match tokio::time::timeout(Duration::from_millis(TASK_TIMEOUT_MILLIS), fut).await {
+      match time::timeout(Duration::from_millis(TASK_TIMEOUT_MILLIS), fut).await {
         Ok(_) => {}
         Err(_) => eprintln!("periodic task timed out after {}s", TASK_TIMEOUT_MILLIS),
       }
